8063751fba79a15d650d51317e1dc0984d2ed7e4
hit9 <hit9@icloud.com>
2015-05-19 16:07:08 +0800

    Basic setup

---
+++ bin/errordog.js
@@ -0,0 +1,5 @@
+#!/bin/sh
+':' //; exec "$(command -v node)" "--harmony" "$0" "$@"
+
+// about the shebang: we can't pass '--harmony' to node via /usr/bin/env
+require('../index.js');
---
+++ index.js
@@ -0,0 +1,31 @@
+// Notify you on error logs.
+// Copyright (c) Eleme, Inc. <hit9@ele.me>
+
+'use strict';
+
+const co       = require('co');
+const program  = require('commander');
+const logging  = require('logging.js');
+const util     = require('./lib/util');
+const version  = require('./package').version;
+
+const log      = logging.get('errordog');
+global.Promise = require('bluebird').Promise;
+
+co(function *() {
+  // argv parsing
+  program
+    .version(version)
+    .usage('<service> [options]')
+    .option('-c, --config-path <c>', 'config file path')
+    .option('-s, --sample-config', 'generate sample config file')
+    .option('-l, --log-level <l>', 'logging level (1~5 for debug~critical)',
+           function(val) {return (parseInt(val, 10) - 1) % 5 + 1;})
+    .parse(process.argv);
+
+  // init logging
+  log.addRule({name: 'stdout', stream: process.stdout,
+              level: (program.logLevel || 2) * 10});
+}).catch(function(e) {
+  util.fatal(e.stack);
+});
