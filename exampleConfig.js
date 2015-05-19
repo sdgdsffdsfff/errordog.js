0ef3d4a888071281f7f502f4f2688a725cc90a46
hit9 <hit9@icloud.com>
2015-05-19 20:33:39 +0800

    Make a step

---
+++ exampleConfig.js
@@ -0,0 +1,12 @@
+// Example config for https://github.com/eleme/errordog.js,
+// with default values. Copyright (c) Eleme, Inc.
+
+{
+  webapp: {
+    root: '',
+    port: 9527,
+    auth: 'secret',
+  },
+  workspace: '.',
+  alerters: ['./alerters/hipchat'],
+}
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // Notify you on error logs.
-// Copyright (c) Eleme, Inc. <hit9@ele.me>
+// MIT. Copyright (c) Eleme, Inc.
+// https://github.com/eleme/errordog.js
+// Only avaliable on Unix(with GNU tail).
 
 'use strict';
 
 const co       = require('co');
+const extend   = require('extend');
+const fs       = require('fs');
 const program  = require('commander');
 const logging  = require('logging.js');
-const util     = require('./util');
+const config   = require('./lib/config');
+const util     = require('./lib/util');
 const version  = require('./package').version;
 
 const log      = logging.get('errordog');
@@ -18,14 +23,39 @@ co(function *() {
     .version(version)
     .usage('<service> [options]')
     .option('-c, --config-path <c>', 'config file path')
-    .option('-s, --sample-config', 'generate sample config file')
     .option('-l, --log-level <l>', 'logging level (1~5 for debug~critical)',
-           function(val) {return (parseInt(val, 10) - 1) % 5 + 1;})
+           function(val) {return Math.max(Math.min(parseInt(val, 10), 5), 1);})
     .parse(process.argv);
 
   // init logging
   log.addRule({name: 'stdout', stream: process.stdout,
               level: (program.logLevel || 2) * 10});
+
+  // update default config
+  if (typeof program.configPath !== 'undefined') {
+    log.debug('load config from %s', program.configPath);
+    var content = fs.readFileSync(program.configPath).toString();
+    extend(config, eval('var _; _ = ' + content));
+  }
+
+  // no service name
+  var name = program.args[0];
+  if (!name) {
+    return program.help();
+  }
+
+  // index service
+  var service = {
+    sentry: require('./lib/sentry'),
+    webapp: require('./lib/webapp'),
+  }[name];
+  if (!service) {
+    // invalid service name
+    program.help();
+  }
+
+  // yield service
+  yield service.serve();
 }).catch(function(e) {
   util.fatal(e.stack);
 });
