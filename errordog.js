b0d7db40d2e4a9685f0221a98c5cdbe15d2c955c
hit9 <hit9@icloud.com>
2015-05-20 20:17:24 +0800

    Minor fixes

--- errordog.js
+++ errordog.js
@@ -12,22 +12,33 @@ const config   = require('./lib/config');
 const util     = require('./lib/util');
 const version  = require('./package').version;
 const log      = logging.get('errordog');
+
 global.Promise = require('bluebird').Promise;
 
+const stderr   = {
+  name: 'stderr',
+  stream: process.stderr,
+  level: logging.INFO,
+};
+
 co(function *() {
-  log.addRule({name: 'stderr', stream: process.stderr});
+  log.addRule(stderr);
+
   // argv parsing
   program
     .version(version)
     .usage('<config>')
     .parse(process.argv);
+
   // update config
   var path = program.args[0];
   if (!path)
     return program.help();
+
   extend(config, require(path));
+
   // logging level
-  log.getRule('stderr').level = logging[config.logging];
+  stderr.level = logging[config.logging];
 }).catch(function(err) {
   return util.fatal(err);
 });
