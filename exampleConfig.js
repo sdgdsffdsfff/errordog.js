60d0656637dc15be50a0d2d7dd13b156c602c36c
hit9 <hit9@icloud.com>
2015-05-20 16:30:44 +0800

    New error dog

--- exampleConfig.js
+++ exampleConfig.js
@@ -1,12 +1,10 @@
 // Example config for https://github.com/eleme/errordog.js,
-// with default values. Copyright (c) Eleme, Inc.
 
-{
-  webapp: {
-    root: '',
-    port: 9527,
-    auth: 'secret',
-  },
-  workspace: '.',
-  alerters: [['./alerters/console']],
-}
+exports = module.exports = {
+  channels: [
+    {
+      channel: require('../alerters/console'),
+      settings: {},
+    }
+  ]
+};
