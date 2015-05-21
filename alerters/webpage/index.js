2eae891983ad75780a139bd75dff7a5b4f341fb1
hit9 <hit9@icloud.com>
2015-05-21 19:28:15 +0800

    Basic webpage ok

--- alerters/webpage/index.js
+++ alerters/webpage/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const cluster  = require('cluster');
 const http     = require('http');
 const logging  = require('logging.js');
 const koa      = require('koa');
@@ -10,13 +9,17 @@ const path     = require('path');
 const route    = require('koa-route');
 const static_  = require('koa-static');
 const socketio = require('socket.io');
+const util     = require('util');
 
 const log      = logging.get('errordog.webpage');
 const pstatic  = path.join(__dirname, 'static');
 const pview    = path.join(__dirname, 'view');
 const loader   = new nunjucks.FileSystemLoader(pview);
 const env      = new nunjucks.Environment(loader);
+const targets  = [];
 
+var host;   // domain or ip
+var port;   // server port
 var root;   // root url prefix
 var app;    // koa app instance
 var server; // http server instance
@@ -31,7 +34,7 @@ var url = function(route, params) {
     var s = path.join('/', root, route);
     if (params) {
       var pairs = [];
-      for (key in params)
+      for (var key in params)
         pairs.push([key, params[key]]
                      .map(encodeURIComponent)
                      .join('='));
@@ -48,8 +51,13 @@ var render = function (tpl, ctx) {
   }
 };
 
+// @route '/:name' & '/'
 var index = function *(name) {
-  this.body = 'hello world';
+  if (typeof name !== 'string') {
+    this.body = yield render('index.html');
+  } else {
+    this.body = yield render('target.html', {name: name});
+  }
 };
 
 exports.name = 'webpage';
@@ -57,17 +65,35 @@ exports.name = 'webpage';
 // initialize this alerter with its global settings
 exports.init = function(settings) {
   root = settings.root || '';
+  host = settings.host || 'localhost';
+  port = settings.port || 9527;
   env.addGlobal('url', url);
+  env.addGlobal('path', path);
+  env.addGlobal('host', host);
+  env.addGlobal('port', port);
+  env.addGlobal('root', root);
+  env.addGlobal('targets', targets);
   app = koa();
-  app.use(mount(url('/public', pbldir)));
-  app.use(mount(url('/:name', index)));
+  app.use(mount(url('/static'), static_(pstatic)));
+  app.use(route.get(url('/'), index));
+  app.use(route.get(url('/:name'), index));
   server = http.createServer(app.callback());
   io = socketio(server);
-  server.listen(settings.port || 9527);
+  server.listen(port);
 };
 
 // connect this alerter to `target` with this target's `settings`
 // for this alerter.
 exports.connect = function(target, settings) {
-
+  targets.push(target);
+  var socket = io.of(url('/io/' + target.name));
+  target.emitter.on('alert', function(level, lines) {
+    socket.emit('data', {
+      count: lines.length,
+      level: level,
+      lines: lines.slice(0, 100),  // max 100 lines
+      datetime: new Date().toString(),
+      interval: target.interval,
+    });
+  });
 };
---
+++ alerters/webpage/static/script.js
@@ -0,0 +1,58 @@
+(function() {
+  this.init = function(host, port, root, name) {
+    var addr;
+
+    if (root.length > 0) {
+      addr = sprintf('http://{0}:{1}/{2}/io/{3}',
+                      host, port, root, name);
+    } else {
+      addr = sprintf('http://{0}:{1}/io/{2}',
+                     host, port, name);
+    }
+
+    var socket = io(addr);
+    var maxCount = 50;
+    var curCount = 0;
+
+    socket.on('data', function(data) {
+      var color;
+      var placeholder = $('ul.main li.placeholder');
+      var child = placeholder.clone();
+      switch(data.level) {
+        case 0:
+          color = 'gray';
+          break;
+        case 1:
+          color = 'yellow';
+          break;
+        case 2:
+          color = 'red';
+          break;
+      }
+      child.attr('class', 'item ' + color);
+      child.find('span p.datetime').text(data.datetime.slice(0, 24));
+      child.find('span p.message').text(sprintf('({0}) {1} errors in {2} secs',
+                                                name, data.count, data.interval));
+      child.find('pre code').text(data.lines.join('\n'));
+      hljs.highlightBlock(child.find('pre code')[0]);
+      $('ul.main').append(child);
+      curCount += 1;
+
+      while (curCount > maxCount) {
+        $('ul.main li.item').first().remove();
+        curCount -= 1;
+      }
+
+      $('body').scrollTop($('body')[0].scrollHeight);
+    });
+  };
+})(this);
+
+// help to sprintf a string
+function sprintf() {
+  var fmt = [].slice.apply(arguments, [0, 1])[0];
+  var args = [].slice.apply(arguments, [1]);
+  return fmt.replace(/{(\d+)}/g, function(match, idx) {
+    return typeof args[idx] != 'undefined'? args[idx] : match;
+  });
+}
